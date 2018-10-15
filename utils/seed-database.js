97c8496743b2800f437d49c587d5e8bb421fee2f
Clinton Owen <clint@clintonowen.com>
2018-10-15 13:31:10 -0500

    Create User seed data & hash-password utility

---
+++ utils/seed-database.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { DATABASE_URL } = require('../config');
+
+const User = require('../models/user');
+
+const seedUsers = require('../db/seed/users');
+
+console.log(`Connecting to mongodb at ${DATABASE_URL}`);
+mongoose.connect(DATABASE_URL)
+  .then(() => {
+    console.info('Dropping Database');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => {
+    console.info('Seeding Database');
+    return Promise.all([
+      User.insertMany(seedUsers),
+      User.createIndexes(),
+    ]);
+  })
+  .then(() => {
+    console.info('Disconnecting');
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+    return mongoose.disconnect();
+  });
