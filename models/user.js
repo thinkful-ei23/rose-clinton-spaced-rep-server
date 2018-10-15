1ca1927701bc2011d84bcfb252c4cf7bc8eeaf2c
Clinton Owen <clint@clintonowen.com>
2018-10-15 13:29:44 -0500

    Create User schema, Install auth dependencies, Update config.js

---
+++ models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+// ===== Define UserSchema & UserModel =====
+const schema = new mongoose.Schema({
+  firstname: { type: String, required: true, default: '' },
+  lastname: { type: String, default: '' },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+schema.set('toObject', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+// Note: Use `function` (not an `arrow function`) to allow setting `this`
+schema.methods.validatePassword = function (pwd) {
+  const currentUser = this;
+  return bcrypt.compare(pwd, currentUser.password);
+};
+
+schema.statics.hashPassword = function (pwd) {
+  return bcrypt.hash(pwd, 10);
+};
+
+module.exports = mongoose.model('User', schema);
