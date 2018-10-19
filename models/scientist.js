d445194a8bcc9ba4fd3dadb953e3b091fe80f400
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-19 13:07:12 -0500

    added links

--- models/scientist.js
+++ models/scientist.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   name: { type: String, required: true, default: '' },
   info: { type: String, required: true, default: '' },
-  photo: { type: String, required: true, default: '' }
+  photo: { type: String, required: true, default: '' },
+  attr: { type: String, required: true, default: '' },
+  link: { type: String, default: '' },
 });
 
 // Transform output during `res.json(data)`, `console.log(data)` etc.
